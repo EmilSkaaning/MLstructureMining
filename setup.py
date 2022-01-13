e8e790fc9efce5fde62b655ef9e760b0fb503101
EmilSkaaning <thygesen1212@gmail.com>
2022-01-13 15:56:25 +0100

    cli fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ciff',
-    version='0.0.1',
+    version='0.1.0',
     author='Emil T. S. Kjaer',
     author_email='etsk@chem.ku.dk',
     url='',
