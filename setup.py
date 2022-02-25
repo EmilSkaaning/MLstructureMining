4a25e71f3c910851069960c1addd13defa32f52b
EmilSkaaning <thygesen1212@gmail.com>
2022-02-25 17:30:37 +0100

    Changed format of data files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ciff',
-    version='0.1.0',
+    version='0.2.0',
     author='Emil T. S. Kjaer',
     author_email='etsk@chem.ku.dk',
     url='https://github.com/EmilSkaaning/ciff',
@@ -28,12 +28,14 @@ setup(
     include_package_data = True,
     zip_safe=False,
 
-    install_requires = [
-        'scipy',
-        'pandas',
-        'xgboost',
-        'tqdm',
-        'matplotlib'
+    install_requires=[
+        'scipy==1.7.3',
+        'pandas==1.3.5',
+        'xgboost==1.5.2',
+        'tqdm==4.62.3',
+        'numpy==1.21.5',
+        'matplotlib==3.5.1',
+        'h5py==3.6.0',
     ],
 )
 
